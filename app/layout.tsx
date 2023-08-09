45a509543e0d0858c11973a7febb83c8c6c5063c
Dimas <you@example.com>
2023-08-09 22:12:28 +0400

    remove metadata from pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,7 @@
 import './globals.scss'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Dima Metelev',
-  description: 'Dmitry Metelev`s homepage',
-}
 
 export default function RootLayout({
   children,
