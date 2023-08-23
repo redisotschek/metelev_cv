8f6a15a8e300cd0831a9cf0aa1aecf75edc9ed79
Dimas <you@example.com>
2023-08-24 00:18:18 +0400

    FIX EVERYTHING AGAIN

--- pages/underconstruction.tsx
+++ pages/underconstruction.tsx
@@ -5,36 +5,37 @@
 // import Link from 'next/link'
 // import { Application } from 'pixi.js'
 
-// export default function UnderConstruction() {
-//   type AppType = Application<HTMLCanvasElement>
-//   let app = useRef<AppType>();
+export default function UnderConstruction() {
+  //   type AppType = Application<HTMLCanvasElement>
+  //   let app = useRef<AppType>();
 
-//   let catInstance = useRef();
+  //   let catInstance = useRef();
 
-//   useEffect(() => {
-//     const initCat = async () => {
-//       const {SmartCat} = await import('cat_module');
-//       app.current = new Application({
-//         resizeTo: window,
-//         backgroundAlpha: 0,
-//       });
+  //   useEffect(() => {
+  //     const initCat = async () => {
+  //       const {SmartCat} = await import('cat_module');
+  //       app.current = new Application({
+  //         resizeTo: window,
+  //         backgroundAlpha: 0,
+  //       });
 
-//       document.body.appendChild(app.current.view);
-//       catInstance.current = new SmartCat(app.current, document.body);
-//     }
-//     initCat();
-//   }, []);
+  //       document.body.appendChild(app.current.view);
+  //       catInstance.current = new SmartCat(app.current, document.body);
+  //     }
+  //     initCat();
+  //   }, []);
 
-//   return (
-//     <main className={styles.main}>
-//       <div className={styles.infoText}>
-//           <h1>
-//             Site is Under Construction
-//           </h1>
-//           <p>
-//             <Link href='/cv' prefetch>Would you like to see my CV?</Link>
-//           </p>
-//       </div>
-//     </main>
-//   )
-// }
+  return (
+    <></>
+    // <main className={styles.main}>
+    //   <div className={styles.infoText}>
+    //       <h1>
+    //         Site is Under Construction
+    //       </h1>
+    //       <p>
+    //         <Link href='/cv' prefetch>Would you like to see my CV?</Link>
+    //       </p>
+    //   </div>
+    // </main>
+  );
+}
