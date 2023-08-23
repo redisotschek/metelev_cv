be4d56f060c22aab2c4e08fb3ebe21b90cfe3b23
Dimas <you@example.com>
2023-08-23 23:28:31 +0400

    add colors, add photo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Scroller from '@/components/scroller/Scroller';
 import { HomeSectionContent } from '@/components/sections/HomeSectionContent';
+import Link from 'next/link';
 import { useEffect } from 'react';
 
 const sections = [
@@ -9,33 +10,40 @@ const sections = [
     id: 'home',
     content: HomeSectionContent(),
   },
-  {
-    title: 'Experience',
-    id: 'experience',
-    content: '',
-  },
-  {
-    title: 'Projects',
-    id: 'projects',
-    content: '',
-  },
-  {
-    title: 'Contacts',
-    id: 'contacts',
-    content: '',
-  },
+  // {
+  //   title: 'Experience',
+  //   id: 'experience',
+  //   content: '',
+  // },
+  // {
+  //   title: 'Projects',
+  //   id: 'projects',
+  //   content: '',
+  // },
+  // {
+  //   title: 'Contacts',
+  //   id: 'contacts',
+  //   content: <footer className="absolute bottom-0">no rights reserved</footer>,
+  // },
 ];
 
 export default function Home(props) {
-  useEffect(() => {
-    // var screenOrientation = window.screen.orientation;
-    // if (screenOrientation.lock) {
-    //   screenOrientation.lock('landscape');
-    // }
-  }, []);
   // const UnderConstructionNoSSR = dynamic(() => import('@/pages/underconstruction'), {ssr: false})
   return (
-    <Scroller sections={sections}></Scroller>
-    // <UnderConstructionNoSSR />
+    <>
+      {/* <header className="fixed w-full flex flex-row justify-around py-2 top-0 z-50">
+        {sections.map((section: any) => {
+          return (
+            <div key={section.id}>
+              <Link href={`#${section.id}`}>
+                <span>{section.title}</span>
+              </Link>
+            </div>
+          );
+        })}
+      </header> */}
+      <Scroller sections={sections}></Scroller>
+      {/* <UnderConstructionNoSSR /> */}
+    </>
   );
 }
--- components/sections/HomeSectionContent.tsx
+++ components/sections/HomeSectionContent.tsx
@@ -1,24 +1,79 @@
 import styles from './home.module.scss';
 import Image from 'next/image';
+import Link from 'next/link';
+
+import mePixel from '@/app/assets/images/big.png';
+
+const networks = [
+  {
+    name: 'linkedin',
+    pathToIcon: 'linkedin.png',
+    link: 'https://www.linkedin.com/in/dmitrymetelev/',
+  },
+  {
+    name: 'github',
+    pathToIcon: 'github.png',
+    link: 'https://github.com/redisotschek/',
+  },
+  {
+    name: 'telegram',
+    pathToIcon: 'telegram.png',
+    link: 'https://t.me/gayboyberserker/',
+  },
+];
+
+const getNetworkLink = ({ name, pathToIcon, link }) => {
+  const iconsPrefix = '/icons/';
+  const iconSize = 30;
+  return (
+    <Link href={link} key={name} target="_blank">
+      <Image
+        src={`${iconsPrefix}${pathToIcon}`}
+        width={iconSize}
+        height={iconSize}
+        alt={name}
+      ></Image>
+    </Link>
+  );
+};
 
 export const HomeSectionContent = () => {
   return (
-    <div className={`section flex items-center justify-center h-full`}>
+    <div
+      className={`section flex flex-col items-center h-full md:w-9/12 gap-5 m-auto justify-center`}
+    >
       <div
-        className={`flex flex-row items-center gap-5 justify-between md:w-9/12`}
+        className={`${styles.card} py-12 px-12 card__text flex flex-row gap-4 items-center justify-around`}
       >
-        <div className={`${styles.card} py-12 px-12 card__text`}>
+        <div>
           <h1 className="text-xl font-bold text-left">
-            Hi, I'm <span className={`${styles.blueText}`}>Dima</span>
+            Hi! I'm <span className={`${styles.blueText}`}>Dima</span>
           </h1>
           <h2>
             I'm a <span className={`${styles.redText}`}>software engineer</span>{' '}
-            with 7 years of experience
+            with 7 years of experience in{' '}
+            <span className={`${styles.greenText}`}>frontend</span> and{' '}
+            <span className={`${styles.greenText}`}>fullstack</span>{' '}
+            development.
           </h2>
         </div>
         <div>
-          <Image src="/me.jpg" width={300} height={300} alt="me"></Image>
+          <Image src={mePixel} width={400} height={400} alt="me"></Image>
         </div>
+      </div>
+      <div
+        className={
+          'flex flex-row gap-5 justify-center items-center w-full text-white'
+        }
+      >
+        {networks.map(getNetworkLink)}
+        <Link
+          href="/Metelev_CV.pdf"
+          target="_blank"
+          className="p-2 bg-blue-500 rounded-md"
+        >
+          Download CV
+        </Link>
       </div>
     </div>
   );
