26f81c5ece8497cd7a78fe60341e941ee54e55e6
Dimas <you@example.com>
2023-08-24 00:02:17 +0400

    fix scripts

--- scripts/copy-assets.mjs
+++ scripts/copy-assets.mjs
@@ -1,16 +1,28 @@
+// usage: "postinstall": "node ./scripts/copy-assets.js"
+
 import { cp } from 'fs/promises';
 import { join } from 'path';
 
 const copyAssets = async () => {
-    try {
-        await cp(join(process.cwd(), "node_modules", "cat_module", "dist", "cat_module", "assets"),
-            join(process.cwd(), 'public', 'cat_module', "assets"), {
-            recursive: true,
-        });
-        console.log("Assets copied successfully!");
-    } catch (error) {
-        console.error(error);
-    }
-}
+  try {
+    await cp(
+      join(
+        process.cwd(),
+        'node_modules',
+        'cat_module',
+        'dist',
+        'cat_module',
+        'assets',
+      ),
+      join(process.cwd(), 'public', 'cat_module', 'assets'),
+      {
+        recursive: true,
+      },
+    );
+    console.log('Assets copied successfully!');
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 await copyAssets();
